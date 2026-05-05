fe0dea810a119f261285290518416c0c6a04266a
Peehu Mishra <peehumishra1308@gmail.com>
2026-05-05 23:17:52 +0530

    Update post.controller.js

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -10,13 +10,25 @@ const createPost=async(req,res)=>{
         const post=await Post.create({
             name,description,age
         });
-        res.status(200).json({message:"post created successfully"});
+        res.status(200).json({message:"post created successfully".post});
     }
     catch(err){
         console.log(err);
     }
 }
 
+const getPosts=async(req,res)=>{
+    try{
+        const posts=await Post.find();
+        res.status(200).json(posts);
+    }
+    catch(err){
+        console.log(`there is an error${err}`)
+    }
+}
+
 export {
-    createPost
+    createPost,
+    getPosts
 };
+
