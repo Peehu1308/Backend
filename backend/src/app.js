e3f2eb0b04e2707f1349e3c05e30fbc4f67c1f80
Peehu Mishra <peehumishra1308@gmail.com>
2026-05-05 23:07:44 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,10 +5,12 @@ app.use(express.json());
 
 
 // routes import
+import usePost from "./routes/post.route.js";
 import userRouter from "./routes/user.route.js";
 
 //  routes declaration
 app.use("/api/v1/users",userRouter);
+app.use("/api/v1/post",usePost);
 
 
 export default app;
