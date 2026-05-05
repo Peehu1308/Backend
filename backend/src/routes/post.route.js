763514d1ae98d37895bcea1d94002d647dd26dcc
Peehu Mishra <peehumishra1308@gmail.com>
2026-05-05 23:17:55 +0530

    Update post.route.js

--- backend/src/routes/post.route.js
+++ backend/src/routes/post.route.js
@@ -1,7 +1,8 @@
-import {Router} from "express";
-import { createPost } from "../controllers/post.controller.js";
+import { Router } from "express";
+import { createPost, getPosts } from "../controllers/post.controller.js";
 
 const router=Router();
 router.route("/post").post(createPost);
+router.route("/getposts").get(getPosts);
 
 export default router;
