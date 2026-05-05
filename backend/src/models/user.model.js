f0583735cceda2844143c41346863ac693865074
Peehu Mishra <peehumishra1308@gmail.com>
2026-05-05 22:53:33 +0530

    Update user.model.js

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -32,11 +32,10 @@ const userSchema=new Schema(
 )
 
 // before saving any password we need to hash it
-userSchema.pre("save",async function(next){
-    if (!this.isModified("password"))return next();
-    this.password=await bcrypt.hash(this.password,10);
+userSchema.pre("save", async function () {
+    if (!this.isModified("password")) return;
 
-    next();
+    this.password = await bcrypt.hash(this.password, 10);
 });
 
 // compare passwords
