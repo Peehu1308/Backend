193d8b0b9e451d1f9f2b75ae9bb87df2e360a149
Peehu Mishra <peehumishra1308@gmail.com>
2026-05-05 23:30:17 +0530

    Update post.controller.js

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -27,8 +27,30 @@ const getPosts=async(req,res)=>{
     }
 }
 
+const updatePost=async(req,res)=>{
+    try{
+        // validation to check if empty body
+
+        // below line gives just the keys
+        if(Object.keys(req.body).length===0){
+            return res.status(400).json({message:"no data for update"});
+
+        }
+
+        const post=await Post.findByIdAndUpdate(req.params.id,req.body,
+            {new:true}
+        );
+        if(!post)return res.status(404).json({message:"post not found"});
+        res.status(200).json({message:"post updated",post});
+
+    }
+    catch(err){
+        console.log(`there is an error${err}`)
+    }
+}
 export {
     createPost,
-    getPosts
+    getPosts,
+    updatePost
 };
 
